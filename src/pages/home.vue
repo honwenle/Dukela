caedb8f33ccfc1328b2d6b7f52904011bfbcb558
Noclip <noclip@foxmail.com>
2018-05-18 15:58:15 +0800

    首页界面改

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,18 +4,31 @@
       <img src="../assets/banenr.png" alt="">
     </div>
     <div class="list">
-      <scroller ref="vuxscroller" lock-x use-pulldown @on-pulldown-loading="loadData" height="-230px">
+      <scroller
+        ref="vuxscroller"
+        lock-x
+        use-pulldown
+        @on-pulldown-loading="loadData"
+        height="-230px"
+        :pulldown-config="{downContent: '下拉刷新', upContent: '释放后更新', loadingContent: '加载中'}">
         <div style="padding: 0 16px;">
           <div class="home-item" v-for="item in dataList" :key="item.id">
-            <div class="flex space-between">
-              <div>{{item.name}}</div>
-              <div>
-                <span class="btn-sm" @click="goGoods(item.id)">立即抢购</span>
-              </div>
+            <div class="bb" style="padding: 6px 15px">
+              <font-icon name="home1" color="#6e9cff"></font-icon>
+              {{item.name}}
             </div>
-            <div class="flex space-between">
-              <div class="blue">{{item.price}}元/小时</div>
-              <div>剩余{{item.surplus}}份</div>
+            <div class="flex flex-center" style="padding: 10px 0 7px 15px">
+              <div class="flex-1">
+                <div class="gray">价格（元/小时）</div>
+                <div class="big-num">3.00</div>
+              </div>
+              <div class="flex-1 border-left">
+                <div class="gray"><font-icon name="rmb"></font-icon>已出售<span class="orange">1000份</span></div>
+                <div class="gray"><font-icon name="kucun"></font-icon>剩余100000份</div>
+              </div>
+              <div class="flex-1 center">
+                <div class="btn-outline">立即抢购</div>
+              </div>
             </div>
           </div>
         </div>
@@ -122,7 +135,7 @@ export default {
 <style scoped>
 .list{
   top: 180px;
-  height: calc(100vh - 230px);
+  height: calc(100vh - 180px);
   width: 100%;
   box-sizing: border-box;
   position: absolute;
@@ -137,5 +150,21 @@ export default {
 .top img{
   width: 100%;
   height: 100%;
+}
+.home-item{
+  padding: 5px;
+  font-size: 12px;
+  white-space: nowrap;
+}
+.big-num{
+  font-size: 35px;
+  color: #ff5400;
+}
+.border-left{
+  padding-left: 18px;
+  border-left: 1px solid #eee;
+}
+.iconfont{
+  margin-right: 10px;
 }
 </style>
