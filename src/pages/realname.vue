48732a6c745c85d49d6cd61de0642cba8f22e6c3
Noclip <noclip@foxmail.com>
2018-05-31 11:09:42 +0800

    实名认证

--- src/pages/realname.vue
+++ src/pages/realname.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="center">
+  <div class="center fixed-bottom">
     <d-header :pervent="true">实名认证</d-header>
     <div class="r-box flex">
       <div class="step flex-1" :class="step ? 'disable' : 'theme-color'">
@@ -16,23 +16,23 @@
       <div class="r-box">
         <div>请上传身份证正面</div>
         <div class="img-upload">
-          <img src="../assets/realname1.png" alt="">
-          <input type="file" id="idcard" @change="updateIdcard">
+          <img :src="img1" alt="">
+          <input type="file" @change="updateIdcard">
         </div>
       </div>
       <div class="r-box">
         <div style="padding-bottom: 15px">请确认您的信息</div>
         <div class="login-input">
           <font-icon name="key"></font-icon>
-          <input v-model="name" type="password" placeholder="输入姓名">
+          <input v-model="name" disabled placeholder="输入姓名">
         </div>
         <div class="login-input">
           <font-icon name="key"></font-icon>
-          <input v-model="idcard" type="password" placeholder="输入身份证号">
+          <input v-model="idcard" disabled placeholder="输入身份证号">
         </div>
       </div>
       <div class="btn-full flex">
-        <div class="flex-1 theme-color fff-bg" @click="nextStep">跳过</div>
+        <div class="flex-1 theme-color fff-bg" @click="$router.go(-2)">跳过</div>
         <div class="flex-1 theme-bg" @click="nextStep">下一步</div>
       </div>
     </template>
@@ -40,21 +40,40 @@
       <div class="r-box">
         <div>请上传个人正面照片</div>
         <div class="img-upload">
-          <img src="../assets/realname1.png" alt="">
-          <input type="file">
+          <img :src="img2" alt="">
+          <input type="file" @change="updateIdcard">
         </div>
-        <div class="btn-full theme-bg" @click="nextStep">确定</div>
+        <div class="btn-full theme-bg" @click="submitCheck">确定</div>
       </div>
     </template>
+    <confirm v-model="show1"
+      confirm-text="设置交易密码"
+      cancel-text="跳过"
+      @on-cancel="$router.go(-2)"
+      @on-confirm="goSetPwd">
+        <div>
+          <font-icon name="dui" fontsize="40px" color="#88c66e"></font-icon>
+        </div>
+        <div>实名认证成功！</div>
+        <div>设置交易密码后方可购买！</div>
+    </confirm>
   </div>
 </template>
 <script>
+import {XDialog, Confirm} from 'vux'
 export default {
+  components: {
+    XDialog, Confirm
+  },
   data() {
     return {
       step: 0,
       name: '',
-      idcard: ''
+      idcard: '',
+      img1: require('../assets/realname1.png'),
+      img2: require('../assets/realname1.png'),
+      imgPath: '',
+      show1: false
     }
   },
   computed: {
@@ -65,16 +84,57 @@ export default {
   methods: {
     updateIdcard(e) {
       let formdata = new FormData()
-      formdata.append('file', e.target.files[0])
-      this.$http.post({
+      let reader = new FileReader()
+      reader.onload = (e) => {
+        if (this.step == 0) {
+          this.img1 = e.target.result
+        } else if (this.step == 1) {
+          this.img2 = e.target.result
+        }
+      }
+      let photo = e.target.files[0]
+      reader.readAsDataURL(photo)
+      formdata.append('file', photo)
+      this.$http({
+        method: 'post',
         isUpload: true,
         url: 'Upload/BatchUpload',
         data: formdata
-      }).then((res)=>{console.log(res)})
+      }).then(({data}) => {
+        if (data.Code == 1) {
+          this.imgPath = data.FileList
+          this.step == 0 && this.submitCheck()
+        } else {
+          this.$vux.toast.text(data.Message)
+        }
+      })
+    },
+    async submitCheck() {
+      let {data} = await this.$http.post('UserBaiduApi/Addinfo', {
+        ApiType: this.step + 1,
+        ImageUrl: this.imgPath,
+        UserName: this.name,
+        IDCard: this.idcard
+      })
+      if (data.Code == 1) {
+        if (this.step == 0) {
+          this.name = data.Model.RealName
+          this.idcard = data.Model.CardID
+        } else if (this.step == 1) {
+          this.show1 = true
+        }
+      } else {
+        this.$vux.toast.text(data.Message)
+      }
     },
     nextStep() {
-      this.step ++
-    }
+      if (this.name && this.idcard) {
+        this.step ++
+      } else {
+        this.$vux.toast.text('请先通过身份证认证')
+      }
+    },
+    goSetPwd() {}
   }
 }
 </script>
--- src/scripts/http.js
+++ src/scripts/http.js
@@ -5,9 +5,14 @@ axios.defaults.baseURL = url.baseURL
 url.isTest || (axios.defaults.withCredentials = true)
 axios.interceptors.request.use(function (config) {
   config.data = config.data || {}
-  config.data.DeviceID = (window.api && window.api.deviceId) || 'TestDeviceID'
-  config.data.UserKey = localStorage.getItem('UserKey') || ''
-  if (!config.isUpload) {
+  let deviceId = (window.api && window.api.deviceId) || 'TestDeviceID'
+  let UserKey = localStorage.getItem('UserKey') || ''
+  if (config.isUpload) {
+    config.data.append('DeviceID', deviceId)
+    config.data.append('UserKey', UserKey)
+  } else {
+    config.data.DeviceID = deviceId
+    config.data.UserKey = UserKey
     config.data = qs.stringify(config.data)
   }
   return config
