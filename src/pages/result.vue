0b5b6255f45acf677960729ddd38d733d133f679
Noclip <noclip@foxmail.com>
2018-06-13 16:18:23 +0800

    结果页面返回首页

--- src/pages/result.vue
+++ src/pages/result.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="bg-fff center page--result">
     <d-header>支付订单</d-header>
-    <div style="padding: 30px 0">
+    <div style="padding: 30px 15%">
       <font-icon :name="status ? 'dui': 'ku'" fontsize="60px" :color="status ? '#88c66e': '#bbb'"></font-icon>
       <div style="margin: 0 0 30px">
         {{title}}<br>{{content}}
       </div>
-      <div class="w40 btn-outline" @click="$router.push('my-order')">查看订单</div>
-      <div style="margin-left: 20px;" class="w40 btn-outline" @click="$router.push('my-goods')" v-if="type == 0">查看商品</div>
+      <div class="flex space-between">
+        <div class="btn-outline" @click="$router.push('my-order')">查看订单</div>
+        <div class="btn-outline" @click="$router.go(-3-type)">返回首页</div>
+      </div>
+      <div v-if="type == 0" class="btn-main w100p" @click="$router.push('my-goods')">查看商品</div>
     </div>
   </div>
 </template>
@@ -23,8 +26,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-.btn-outline{
-  padding: 2px 40px;
-}
-</style>
