de070c3f32c2fb625784a4e8619be4a91dd32677
Noclip <noclip@foxmail.com>
2018-07-19 16:17:06 +0800

    ajax加载

--- src/scripts/http.js
+++ src/scripts/http.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
 import qs from 'qs'
 import url from './url'
+import Vue from 'vue'
 axios.defaults.baseURL = url.baseURL
 url.isTest || (axios.defaults.withCredentials = true)
 axios.interceptors.request.use(function (config) {
+  Vue.$vux.loading.show({
+    text: '请稍等'
+  })
   config.data = config.data || {}
   let deviceId = (window.api && window.api.deviceId) || 'TestDeviceID'
   let UserKey = localStorage.getItem('UserKey') || ''
@@ -17,6 +21,13 @@ axios.interceptors.request.use(function (config) {
   }
   return config
 }, function (error) {
+  return Promise.reject(error)
+})
+axios.interceptors.response.use(function (response) {
+  Vue.$vux.loading.hide()
+  return response
+}, function (error) {
+  Vue.$vux.loading.hide()
   return Promise.reject(error)
 })
 export default axios
