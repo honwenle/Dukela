a82317b8d401077becad4edd772205e6c28ffed3
Noclip <noclip@foxmail.com>
2018-08-07 17:20:50 +0800

    字段名

--- src/pages/promotion-record.vue
+++ src/pages/promotion-record.vue
@@ -17,10 +17,10 @@
           <div class="flex-5">
             <div class="flex space-between fz16">
               <div>订单号：{{item.OrderNumber}}</div>
-              <div class="cost-color cost-num">{{item.Amount}}</div>
+              <div class="cost-color cost-num">{{item.AchievementAmount}}</div>
             </div>
             <div class="flex space-between">
-              <div class="gray">交易金额：{{item.OrderAmount}}</div>
+              <div class="gray">交易金额：{{item.Amount}}</div>
               <div class="gray">{{item.CreateTime}}</div>
             </div>
           </div>
