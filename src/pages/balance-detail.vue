ff638d85c40791ba57d19d22cfd16987b5a8c5c3
Noclip <noclip@foxmail.com>
2018-08-23 11:34:06 +0800

    余额明细字段

--- src/pages/balance-detail.vue
+++ src/pages/balance-detail.vue
@@ -6,7 +6,7 @@
         <div class="cost-color cost-num">{{balanceDetail.Amount}}</div>
       </cell>
       <cell title="订单类型" :value="balanceDetail.SourceType | balanceTypeName"></cell>
-      <cell title="时间" :value="balanceDetail.UpdateTime | DATEFORMAT"></cell>
+      <cell title="时间" :value="balanceDetail.CreateTime"></cell>
       <cell title="余额(元)" :value="balanceDetail.AccountBalance"></cell>
       <cell title="状态" :value="balanceDetail.SourceType != 5 ? '已完成' : ['审核中', '审核通过', '提现失败', '已打款'][balanceDetail.Status]"></cell>
       <cell title="原因" v-if="balanceDetail.Remark" :value="balanceDetail.Remark"></cell>
